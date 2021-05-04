71bcf21f794c937c204d8ca5b606fa8f9d413ffa
0x1a0b <65621431+0x1a0b@users.noreply.github.com>
2021-05-04 21:39:21 +0200

    init

---
+++ scripts/google-test.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'Demo test Google' : function (browser) {
+      browser
+        .url('http://www.google.com')
+        .waitForElementVisible('body', 1000)
+        .setValue('input[type=text]', 'nightwatch')
+        .pause(1000)
+        .end();
+    }
+  };
